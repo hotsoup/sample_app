f0dd5d8ee03f4705a88ecbe18e42b482b5471721
hotsoup <wnxhotsoup@gmail.com>
2011-04-17 14:33:35 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '3efe6f547446993c4c1d92d62de67cbdedd10ce1f0183e68553b168ab0cd5f17f0e6c00386741368aa9d00132b7b0defe91dcaa0bd364834b4861363b0c7bd02'
